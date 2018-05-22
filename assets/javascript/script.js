b502480772f010add80ae7cc2de55deabd9aa39b
Allison Hahn <allisonk.hahn@gmail.com>
2018-05-21 21:16:53 -0700

    wrote function for finding gifs based on button value

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,20 @@
-var topics = ["Game of Thrones", "Dogs", "Angels Baseball", "Flowers", "Embroidery"]
+
+
+$(".click").click(function() {
+    var topics = ["Game+of+Thrones", "Dogs", "Angels+Baseball", "Flowers", "Embroidery", "Sushi", "Denim", "Mountains"];
+    var number = parseInt($(this).val());
+    console.log(number);
+    var input = topics[number];
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+ "&api_key=yoY8WGlVRAFrybPEutH2LAhyth9Nagwa&limit=10")
+
+    xhr.done(function(data) { 
+        console.log("success got data", data);
+
+    var gifs = data.data;
+
+    for (i in gifs) {
+        $("#showGifs").prepend("<img src='"+gifs[i].images.original.url+" '/> rating:"+gifs[i].rating)   
+    }
+    }); 
+});
+
